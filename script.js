6b1294325779698bcfb81b33f92bb77bcefbc0af
Anna Volkova <volkovaaaa@yandex-team.ru>
2022-01-19 18:23:50 +0300

    pretty

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 'use strict'
-function translit (text) {
- let result = '';
- let Rus = document.querySelector("#russian-text");
- let Eng = document.querySelector("#english-text");
- const alphabet = {
-    'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
-		'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
-		'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
-		'о': 'o',    'п': 'p',    'р': 'r',    'с': 's',    'т': 't',
-		'у': 'u',    'ф': 'f',    'х': 'h',    'ц': 'c',     'ч': 'ch',
-		'ш': 'sh',   'щ': 'sch',  'ь': '',     'ы': 'y',    'ъ': '',
-		'э': 'e',    'ю': 'yu',   'я': 'ya',
- 
-		'А': 'A',    'Б': 'B',    'В': 'V',    'Г': 'G',    'Д': 'D',
-		'Е': 'E',    'Ё': 'E',    'Ж': 'Zh',   'З': 'Z',    'И': 'I',
-		'Й': 'Y',    'К': 'K',    'Л': 'L',    'М': 'M',    'Н': 'N',
-		'О': 'O',    'П': 'P',    'Р': 'R',    'С': 'S',    'Т': 'T',
-		'У': 'U',    'Ф': 'F',    'Х': 'H',    'Ц': 'C',    'Ч': 'Ch',
-		'Ш': 'Sh',   'Щ': 'Sch',  'Ь': '',     'Ы': 'Y',    'Ъ': '',
-		'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya',   ' ': ' '
-  };
-  for(let i = 0; i < text.length; i++) {
-     let ifUpper = (text[i] === text[i].toUpperCase());
-     if (alphabet[text[i].toLowerCase()] == undefined) {
-       result += word[i];
-     } else {
-       if(ifUpper) {
-         result += alphabet[text[i].toLowerCase()].toUpperCase();
-       } else  result += alphabet[text[i].toLowerCase()];
-     }
-  }
-  return result;
+
+function translit(text) {
+    let result = '';
+    let Rus = document.querySelector("#russian-text");
+    let Eng = document.querySelector("#english-text");
+    const alphabet = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+        'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
+        'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+        'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+        'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+        'ш': 'sh', 'щ': 'sch', 'ь': '', 'ы': 'y', 'ъ': '',
+        'э': 'e', 'ю': 'yu', 'я': 'ya',
+
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D',
+        'Е': 'E', 'Ё': 'E', 'Ж': 'Zh', 'З': 'Z', 'И': 'I',
+        'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N',
+        'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T',
+        'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ц': 'C', 'Ч': 'Ch',
+        'Ш': 'Sh', 'Щ': 'Sch', 'Ь': '', 'Ы': 'Y', 'Ъ': '',
+        'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya', ' ': ' '
+    };
+    for (let i = 0; i < text.length; i++) {
+        let ifUpper = (text[i] === text[i].toUpperCase());
+        if (alphabet[text[i].toLowerCase()] == undefined) {
+            result += word[i];
+        } else {
+            if (ifUpper) {
+                result += alphabet[text[i].toLowerCase()].toUpperCase();
+            } else result += alphabet[text[i].toLowerCase()];
+        }
+    }
+    return result;
 }
 
 function enterStr() {
@@ -39,11 +40,11 @@ function enterStr() {
     let newDiv2 = document.createElement('div');
 
     if (textEntered.length < 12) {
-      newDiv.title = textEntered;
-      newDiv2.title = translit(textEntered);
-      newDiv.style.cursor = "pointer";
-      newDiv2.style.cursor = "pointer";
-      textEntered = textEntered.substring(0, 12) + "...";
+        newDiv.title = textEntered;
+        newDiv2.title = translit(textEntered);
+        newDiv.style.cursor = "pointer";
+        newDiv2.style.cursor = "pointer";
+        textEntered = textEntered.substring(0, 12) + "...";
 
     }
 
@@ -56,19 +57,20 @@ function enterStr() {
 
     document.querySelector('input').value = "";
     return false;
-  }
+}
 
 function init() {
-     let button = document.querySelector("#text-submit");
-     button.addEventListener('click', enterStr);
-     document.querySelector('input').addEventListener('keydown', function(e) {
-       if(e.wich === 13 || e.keyCode === 13 || e.key === 'Enter') {
-         enterStr();
-         e.preventDefault();
-         return false;
-      }
-  });
- } 
+    let button = document.querySelector("#text-submit");
+    button.addEventListener('click', enterStr);
+    document.querySelector('input').addEventListener('keydown', function (e) {
+        if (e.wich === 13 || e.keyCode === 13 || e.key === 'Enter') {
+            enterStr();
+            e.preventDefault();
+            return false;
+        }
+    });
+}
+
 window.onload = init;
   
 
@@ -77,5 +79,4 @@ window.onload = init;
   
     
     
-  
   
